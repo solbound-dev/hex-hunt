fbf4047a7a8681ce4ef8d59ad0d42c0d0b0bf0c0
tsiklic1 <toma.siklic@gmail.com>
2025-09-14 21:16:14 +0200

    vercel try1

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,23 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
+import { Server } from 'http';
+
+let server: Server;
+
+/**
+ * Vercel handler for serverless functions
+ */
+export default async function handler(req: any, res: any) {
+  if (!server) {
+    const app = await NestFactory.create(AppModule);
+    await app.init();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    server = app.getHttpAdapter().getInstance();
+  }
+
+  return server.emit('request', req, res);
+}
 
 const setupFilter = (app: INestApplication) => {
   const { httpAdapter } = app.get(HttpAdapterHost);
