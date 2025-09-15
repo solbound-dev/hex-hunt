ee2be391ff66e32a8d379454d1d3c046bc16c02e
tsiklic1 <toma.siklic@gmail.com>
2025-09-15 14:08:29 +0200

    aaaaaa<<

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,27 +2,6 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
-// import { Server } from 'http';
-
-// let server: Server;
-
-// async function bootstrapServer() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.init();
-//   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-//   return app.getHttpAdapter().getInstance();
-// }
-
-// /**
-//  * Vercel serverless handler
-//  */
-// export default async function handler(req: any, res: any) {
-//   if (!server) {
-//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-//     server = await bootstrapServer();
-//   }
-//   server.emit('request', req, res);
-// }
 
 const setupFilter = (app: INestApplication) => {
   const { httpAdapter } = app.get(HttpAdapterHost);
@@ -39,9 +18,9 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3000;
   if (process.env.NODE_ENV === 'production') {
-    app.listen(port, "0.0.0.0");
+    void app.listen(port, '0.0.0.0');
   } else {
-    app.listen(port);
+    void app.listen(port);
   }
 }
 
