9399df74b87652dc8f4481ff6ea39669765b5910
tsiklic1 <toma.siklic@gmail.com>
2025-10-13 13:15:09 +0200

    add to cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
       'http://localhost:3001',
       'http://localhost:3005',
       'http://localhost:5173',
+      'https://hextraction.xyz',
     ],
     credentials: true,
   });
