4ca2fdf1999c9e32da2478ff35b2b832aafec8ed
tsiklic1 <toma.siklic@gmail.com>
2025-10-20 11:24:54 +0200

    commit for deploy

--- apps/web/src/hooks/game.ts
+++ apps/web/src/hooks/game.ts
@@ -128,6 +128,8 @@ export const useInitializeSockets = (
   return socketRef;
 };
 
+//
+
 export const useTimer = (
   gameState: GameData | undefined,
   socketRef: React.RefObject<Socket<DefaultEventsMap, DefaultEventsMap> | null>,
