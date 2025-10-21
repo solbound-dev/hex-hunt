7a2562df90b349440f3aa771d2b70172cb6a8d52
tsiklic1 <toma.siklic@gmail.com>
2025-10-21 13:35:03 +0200

    styles

--- apps/web/src/pages/FindGamePage/FindGamePage.tsx
+++ apps/web/src/pages/FindGamePage/FindGamePage.tsx
@@ -5,6 +5,12 @@ import { useEffect, useState } from 'react';
 import Button from '../../components/Button';
 import c from './style.module.css';
 import clsx from 'clsx';
+import Modal from '../../components/Modal';
+import ModalButton from '../../components/Modal/ModalButton';
+import ClockIcon from '../../assets/clock.svg';
+import UsersCrownIcon from '../../assets/users-crown.svg';
+import LockIcon from '../../assets/lock.svg';
+import Logo from '../../assets/logo-hex.svg';
 
 const FindGamePage = () => {
   const { isAuthenticated, isCheckingAuth, logoutAndDisconnect } = useAuth();
@@ -28,10 +34,45 @@ const FindGamePage = () => {
   }, [gameId, navigate]);
 
   return (
-    <div className={c.wrapper}>
-      <div className={c.pageWrapper}>
-        <div className={c.header}>Find game</div>
-        {/* <div className={c.buttonGroup}>
+    <div className={c.pageWrapper}>
+      <div className={c.navigation}>
+        <img src={Logo} alt='' />
+      </div>
+      <Modal isOpen={true} setIsOpen={() => {}}>
+        <ul className={c.ul}>
+          <li className={c.li}>
+            <ModalButton
+              handleClick={() => {
+                socketRef.current?.disconnect();
+                socketRef.current?.connect();
+                socketRef.current?.emit('quickJoin', { tier: 1 });
+                navigate('/game');
+              }}
+              icon={ClockIcon}
+              iconSize={20}
+              name='Quick match'></ModalButton>
+          </li>
+          <li className={c.li}>
+            <ModalButton
+              handleClick={() => {
+                socketRef.current?.disconnect();
+                socketRef.current?.connect();
+                socketRef.current?.emit('hostPrivateGame', { tier: 1 });
+                navigate('/game');
+              }}
+              icon={UsersCrownIcon}
+              iconSize={20}
+              name='Host private game'></ModalButton>
+          </li>
+          <li className={c.li}>
+            <ModalButton
+              icon={LockIcon}
+              iconSize={20}
+              name='Join private game'></ModalButton>
+          </li>
+        </ul>
+      </Modal>
+      {/* <div className={c.buttonGroup}>
           <Button
             onClick={() => {
               socketRef.current?.disconnect();
@@ -78,50 +119,48 @@ const FindGamePage = () => {
               </div>
             )}
           </div> */}
-        {/* </div> */}
-        <div className={c.fixedRight}>
-          <Button onClick={() => logoutAndDisconnect()}>Logout</Button>
-          <br />
-          <Button onClick={() => setShowInfo(true)}>Rules</Button>
-        </div>{' '}
-        <div className={clsx(c.infoContainer, { [c.open]: showInfo })}>
-          <Button onClick={() => setShowInfo(false)}>Close</Button>
-          <h2>Rules</h2>
-          <ul>
-            <li>
-              Move: Click an adjacent hex to move your character before the
-              timer runs out.
-            </li>
-            <li>
-              Shoot: Click "Shoot" and select an adjacent hex to fire in that
-              direction.
-            </li>
-            <li>
-              Cards: Collect a card by moving onto it to gain one-time immunity
-              from shots.
-            </li>
-            <li>
-              Win: Collect 3 cards and go to the middle or just be the last one
-              standing
-            </li>
-            <li>
-              Collision: If both players move to the same hex, they bounce back
-              to their previous positions, and the hex is marked as last known
-              for both.
-            </li>
-            <li>
-              Zone: Every 8 moves, the grid shrinks. Players outside the zone
-              die.
-            </li>
-            <li>
-              Visibility: Opponents are only visible at their last known
-              position (when they shoot or collect a card).
-            </li>
-            <li>
-              Turn: Shots resolve after all players make a move (shoot or move).
-            </li>
-          </ul>
-        </div>
+      {/* </div> */}
+      <div className={c.fixedRight}>
+        <Button onClick={() => logoutAndDisconnect()}>Logout</Button>
+        <br />
+        <Button onClick={() => setShowInfo(true)}>Rules</Button>
+      </div>{' '}
+      <div className={clsx(c.infoContainer, { [c.open]: showInfo })}>
+        <Button onClick={() => setShowInfo(false)}>Close</Button>
+        <h2>Rules</h2>
+        <ul>
+          <li>
+            Move: Click an adjacent hex to move your character before the timer
+            runs out.
+          </li>
+          <li>
+            Shoot: Click "Shoot" and select an adjacent hex to fire in that
+            direction.
+          </li>
+          <li>
+            Cards: Collect a card by moving onto it to gain one-time immunity
+            from shots.
+          </li>
+          <li>
+            Win: Collect 3 cards and go to the middle or just be the last one
+            standing
+          </li>
+          <li>
+            Collision: If both players move to the same hex, they bounce back to
+            their previous positions, and the hex is marked as last known for
+            both.
+          </li>
+          <li>
+            Zone: Every 8 moves, the grid shrinks. Players outside the zone die.
+          </li>
+          <li>
+            Visibility: Opponents are only visible at their last known position
+            (when they shoot or collect a card).
+          </li>
+          <li>
+            Turn: Shots resolve after all players make a move (shoot or move).
+          </li>
+        </ul>
       </div>
     </div>
   );
