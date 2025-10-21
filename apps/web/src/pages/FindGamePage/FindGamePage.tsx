8d7443149329a31b0e1b05f374453983c74c8a32
tsiklic1 <toma.siklic@gmail.com>
2025-10-21 15:24:00 +0200

    add rules popup

--- apps/web/src/pages/FindGamePage/FindGamePage.tsx
+++ apps/web/src/pages/FindGamePage/FindGamePage.tsx
@@ -11,6 +11,7 @@ import ClockIcon from '../../assets/clock.svg';
 import UsersCrownIcon from '../../assets/users-crown.svg';
 import LockIcon from '../../assets/lock.svg';
 import Logo from '../../assets/logo-hex.svg';
+import RulesBackground from '../../assets/rules-paper.png';
 
 const FindGamePage = () => {
   const { isAuthenticated, isCheckingAuth, logoutAndDisconnect } = useAuth();
@@ -36,9 +37,24 @@ const FindGamePage = () => {
   return (
     <div className={c.pageWrapper}>
       <div className={c.navigation}>
-        <img src={Logo} alt='' />
+        <div className={c.flex}>
+          <img className={c.logo} src={Logo} alt='logo' />
+          <p className={c.navigationText}>hextraction</p>
+        </div>
+        <div>
+          <button
+            className={clsx(c.rulesButton, { [c.active]: showInfo })}
+            onClick={() => setShowInfo((prev) => !prev)}>
+            Rules
+          </button>
+          <button
+            className={c.logoutButton}
+            onClick={() => logoutAndDisconnect()}>
+            Logout
+          </button>
+        </div>
       </div>
-      <Modal isOpen={true} setIsOpen={() => {}}>
+      <Modal isOpen={!showInfo} setIsOpen={() => {}}>
         <ul className={c.ul}>
           <li className={c.li}>
             <ModalButton
@@ -120,47 +136,53 @@ const FindGamePage = () => {
             )}
           </div> */}
       {/* </div> */}
-      <div className={c.fixedRight}>
+      {/* <div className={c.fixedRight}>
         <Button onClick={() => logoutAndDisconnect()}>Logout</Button>
         <br />
         <Button onClick={() => setShowInfo(true)}>Rules</Button>
-      </div>{' '}
+      </div>{' '} */}
       <div className={clsx(c.infoContainer, { [c.open]: showInfo })}>
-        <Button onClick={() => setShowInfo(false)}>Close</Button>
-        <h2>Rules</h2>
-        <ul>
-          <li>
-            Move: Click an adjacent hex to move your character before the timer
-            runs out.
-          </li>
-          <li>
-            Shoot: Click "Shoot" and select an adjacent hex to fire in that
-            direction.
-          </li>
-          <li>
-            Cards: Collect a card by moving onto it to gain one-time immunity
-            from shots.
-          </li>
-          <li>
-            Win: Collect 3 cards and go to the middle or just be the last one
-            standing
-          </li>
-          <li>
-            Collision: If both players move to the same hex, they bounce back to
-            their previous positions, and the hex is marked as last known for
-            both.
-          </li>
-          <li>
-            Zone: Every 8 moves, the grid shrinks. Players outside the zone die.
-          </li>
-          <li>
-            Visibility: Opponents are only visible at their last known position
-            (when they shoot or collect a card).
-          </li>
-          <li>
-            Turn: Shots resolve after all players make a move (shoot or move).
-          </li>
-        </ul>
+        <div className={c.rulesWrapper}>
+          <img src={RulesBackground} alt='' />
+          <div className={c.infoInnerWrapper}>
+            <h2 className={c.rulesTitle}>Rules</h2>
+            <ul>
+              <li className={c.rulesText}>
+                Move: Click an adjacent hex to move your character before the
+                timer runs out.
+              </li>
+              <li className={c.rulesText}>
+                Shoot: Click "Shoot" and select an adjacent hex to fire in that
+                direction.
+              </li>
+              <li className={c.rulesText}>
+                Cards: Collect a card by moving onto it to gain one-time
+                immunity from shots.
+              </li>
+              <li className={c.rulesText}>
+                Win: Collect 3 cards and go to the middle or just be the last
+                one standing
+              </li>
+              <li className={c.rulesText}>
+                Collision: If both players move to the same hex, they bounce
+                back to their previous positions, and the hex is marked as last
+                known for both.
+              </li>
+              <li className={c.rulesText}>
+                Zone: Every 8 moves, the grid shrinks. Players outside the zone
+                die.
+              </li>
+              <li className={c.rulesText}>
+                Visibility: Opponents are only visible at their last known
+                position (when they shoot or collect a card).
+              </li>
+              <li className={c.rulesText}>
+                Turn: Shots resolve after all players make a move (shoot or
+                move).
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
