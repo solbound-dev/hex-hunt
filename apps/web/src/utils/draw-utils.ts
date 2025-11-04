feee8c893336514c07fd83934fd0deee75b9d01c
tsiklic1 <toma.siklic@gmail.com>
2025-11-04 15:50:18 +0100

    make zone contract faster (after 6 moves)

--- apps/web/src/utils/draw-utils.ts
+++ apps/web/src/utils/draw-utils.ts
@@ -687,7 +687,7 @@ export function repaint(
   );
   if (
     gameState.moves &&
-    (gameState.moves % 8 === 6 || gameState.moves % 8 === 7) &&
+    (gameState.moves % 6 === 5 || gameState.moves % 8 === 4) &&
     gameState.currentRadius > 1
   ) {
     drawZoneContractionWarningIsometric(
