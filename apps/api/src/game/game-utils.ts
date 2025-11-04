c3431ce01f015e48e05875bdbe27e946b8b23982
tsiklic1 <toma.siklic@gmail.com>
2025-11-04 14:09:56 +0100

    remove unused code in game-utils

--- apps/api/src/game/game-utils.ts
+++ apps/api/src/game/game-utils.ts
@@ -1,5 +1,3 @@
-import { Game } from './Game';
-import { DefaultEventsMap, Server } from 'socket.io';
 import { Hex } from './Hex';
 
 export const MAX_PLAYERS = 4;
@@ -9,71 +7,3 @@ export const MOVE_DURATION_IN_SECONDS = 20;
 export function isNeighbor(hex: Hex, other: Hex) {
   return hex.neighbors().some((n) => n.equals(other));
 }
-
-//myb Game
-// export function updateAndEmitGameState(
-//   gameId: string,
-//   game: Game,
-//   websocketServer: Server<
-//     DefaultEventsMap,
-//     DefaultEventsMap,
-//     DefaultEventsMap,
-//     any
-//   >,
-// ) {
-//   game.moves++;
-//   if (game.moves % 8 === 0 && game.currentRadius > 1) {
-//     game.currentRadius--;
-//     game.contractZone();
-//     const zoneAteCard = game.disappearedHexes.some((hex) =>
-//       hex.equals(game.cardPos!),
-//     );
-//     if (zoneAteCard) {
-//       game.spawnCard();
-//     }
-//   }
-
-//   game.players.forEach((p) => {
-//     const playerIsInForbiddenZone = game.disappearedHexes.some((h) => {
-//       h.equals(p.pos);
-//     });
-//     if (playerIsInForbiddenZone) {
-//       console.log(`${p.playerType} died`);
-//       p.isDead = true;
-//     }
-//   });
-
-//   game.players.forEach((p) => {
-//     if (p.cards === 3 && p.pos?.equals(new Hex(0, 0))) {
-//       game.players.forEach((op) => {
-//         if (op.walletId !== p.walletId) {
-//           op.isDead = true;
-//         }
-//       });
-//     }
-//   });
-
-//   const numberOfDeadPlayers = game.players.reduce(
-//     (prev, curr) => prev + (curr.isDead ? 1 : 0),
-//     0,
-//   );
-
-//   if (numberOfDeadPlayers === game.players.length - 1) {
-//     game.players.forEach((p) => {
-//       if (!p.isDead) {
-//         p.won = true;
-//         p.wins++;
-//       }
-//     });
-//   }
-
-//   websocketServer.to(gameId).emit('gameState', game);
-//   game.players.forEach((p) => {
-//     if (!p.justPickedCard) {
-//       p.isImmune = false;
-//     }
-//     p.pendingMove = null;
-//     p.isShooting = null;
-//     p.didJustCollide = false;
-//   });
-// }
