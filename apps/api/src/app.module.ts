246a168cbaf68b40f79a8e67b6f90d1a0574f540
tsiklic1 <toma.siklic@gmail.com>
2025-11-14 15:50:43 +0100

    add history module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule, GameModule],
+  imports: [PrismaModule, AuthModule, UserModule, GameModule, HistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
